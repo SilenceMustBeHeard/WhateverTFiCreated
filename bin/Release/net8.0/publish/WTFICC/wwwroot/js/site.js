ade7a8f722daf382860947c203e2df7b8307ed7e
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2025-01-14 14:23:55 +0200

    Initial commit
    Initial commit

---
+++ bin/Release/net8.0/publish/WTFICC/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿let likeCount = 0;
+
+document.querySelector('.like-button').addEventListener('click', function () {
+    const counterElement = document.getElementById('counter');
+
+    
+    this.classList.toggle('liked');
+
+    if (this.classList.contains('liked')) {
+        likeCount++;
+        this.innerHTML = '<span class="thumb-icon">👍</span> Liked';
+        this.style.backgroundColor = '#4CAF50'; 
+    } else {
+        likeCount--;
+        this.innerHTML = '<span class="thumb-icon">👎</span> Like';
+        this.style.backgroundColor = '#ff4d4d'; 
+    }
+
+    
+    counterElement.textContent = likeCount;
+});
--- obj/Debug/net8.0/WhateverTFICreated.AssemblyInfo.cs
+++ obj/Debug/net8.0/WhateverTFICreated.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhateverTFICreated")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964eb6999ed4f75be3217561ee79fd3abd058fa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhateverTFICreated")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhateverTFICreated")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
